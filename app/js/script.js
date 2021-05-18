cdb585200f9afa5dfed7c44c11fce2db97c6ed6f
Mario Johar <mjohar30@gmail.com>
2021-05-18 18:53:46 -0500

    Transform hamburger menu icon to an "X" icon

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,10 @@
-console.log('Hi')
+const btnHamburger = document.querySelector('#btnHamburger')
+
+btnHamburger.addEventListener('click', function() {
+  console.log('Activated')
+  if (btnHamburger.classList.contains('open')){
+    btnHamburger.classList.remove('open')
+  } else {
+    btnHamburger.classList.add('open')
+  }
+})
