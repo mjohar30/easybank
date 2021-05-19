adace36c07e380f418f68b3c0c19ddff88a32e4f
Mario Johar <mjohar30@gmail.com>
2021-05-18 19:46:24 -0500

    Hamburger icon animations

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,16 @@
 const btnHamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
 
 btnHamburger.addEventListener('click', function() {
   console.log('Activated')
-  if (btnHamburger.classList.contains('open')){
-    btnHamburger.classList.remove('open')
-  } else {
-    btnHamburger.classList.add('open')
+  if (header.classList.contains('open')){ //Close Hamburger menu
+    header.classList.remove('open')
+    overlay.classList.remove('fade-in')
+    overlay.classList.add('fade-out')
+  } else { // Open Hamburger menu
+    header.classList.add('open')
+    overlay.classList.remove('fade-out')
+    overlay.classList.add('fade-in')
   }
 })
